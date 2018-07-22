2ea4648bdc82072cb8f74e0714ad04a8a0fc8639
Shen_Jerry <fast199111@yahoo.com.tw>
2018-07-22 22:38:02 +0800

    view0 畫面

--- src/components/view0.vue
+++ src/components/view0.vue
@@ -5,23 +5,23 @@
     <p>
       Glad to see you here!
     </p>
-    <form>
+    <form class="form-area">
       <div class="form-group">
         <label>Account</label>
-        <input type="email" placeholder="example@eamil.com" v-model="mail"/>
+        <input id="input-email" type="email" placeholder="example@eamil.com" v-model="mail"/>
         <div class="alarm" v-show="mailError">INVALID EMAIL</div>
       </div>
       <div class="form-group">
         <label>Password</label>
-        <input type="password" placeholder="password" v-model="password"/>
+        <input id="input-password" type="password" placeholder="password" v-model="password"/>
         <div class="alarm" v-show="passwordError">MINIMUM 8 CHARACTERS</div>
       </div>
       <div class="form-group">
         <label>Comfirm Password</label>
-        <input type="password" placeholder="password" v-model="password2"/>
+        <input id="input-password2" type="password" placeholder="password" v-model="password2"/>
         <div class="alarm" v-show="password2Error">NOT MATCH</div>
       </div>
-      <input type="button" value="SUBMIT & NEXT" :disabled="formError" @click="send">
+      <input id="button" type="button" value="SUBMIT & NEXT" :disabled="formError" @click="send">
     </form>
     <pre>{{mail}}</pre>
   </div>
@@ -81,8 +81,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.view0 h1 {
+  margin-top: 54px;
+  font-family: "Roboto";
+  font-size: 48px;
+  line-height: 0;
+}
+.view0 p {
+  font-family: "Roboto";
+  font-size: 24px;
+}
 .alarm {
   display: inline;
   background-color: #f5a623;
+}
+.form-area {
+  text-align: start;
+  width: 30%;
+  margin: auto;
+}
+.form-group label {
+  display: block;
+  font-size: 20px;
+  padding: 8px 0;
+}
+#input-email,
+#input-password,
+#input-password2 {
+  width: 100%;
+  height: 52px;
+  border-radius: 8px;
+  border: 2px solid #000;
+}
+input::placeholder {
+  font-size: 20px;
+  box-sizing: border-box;
+  padding: 12px 20px;
+}
+#button {
+  background: #bee1ff;
+  border: 0;
+  width: 100%;
+  height: 52px;
+  border-radius: 8px;
+  margin-top: 40px;
+  font-family: "Roboto" !important;
+  font-size: 20px;
+  color: white;
 }
 </style>
